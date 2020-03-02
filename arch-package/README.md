5d0831219d9bf82adf23316f2b88dff9e2593fea
Sean E. Russell <ser@ser1.net>
2020-03-02 08:48:00 -0600

    Moves all functionality to one script.

--- arch-package/README.md
+++ arch-package/README.md
@@ -1,14 +1,57 @@
-# AUR
+# Arch packager
 
-1.  Update aur/PKGBUILD (`update.sh TAG`)
-2.  namcap PKGBUILD
-3.  makepkg
-4.  makepkg -g
-5.  makepkg --printsrcinfo \> .SRCINFO
-6.  git commit -a
-7.  git push
+Github Action to build (and release) [AUR](https://aur.archlinux.org/) packages for [Arch Linux](https://www.archlinux.org/).
 
+## Usage
+
+Create a workflow file `.github/workflows/post-release.yml` with the content below (replacing or removing the release step args):
+
+```yml
+name: Post release triggers
+
+on: 
+  release:
+    types: [published]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Get tag name
+        shell: bash
+        run: echo "##[set-output name=tag;]$(echo ${GITHUB_REF##*/})"
+        id: tag_name
+
+      - name: Release Arch AUR packages
+        uses: xxxserxxx/actions/arch-package@master
+        with:
+          args: gotop%20-V=${{ steps.tag_name.outputs.tag }}
+        env:
+          VERSION: ${{ steps.tag_name.outputs.tag }}
+
+      - uses: stefanzweifel/git-auto-commit-action@v4.0.0
+        with:
+            commit_message: Update packages to version "${{ steps.tag_name.outputs.tag }}"
+
+            # Optional glob pattern of files which should be added to the commit
+            file_pattern: .
+
+            # Optional commit user and author settings
+            commit_user_name: Tap Updater
+            commit_user_email: ser@ser1.net
+            commit_author: Tap Updater <ser@ser1.net>
 ```
-curl -L https://github.com/xxxserxxx/gotop/archive/v3.3.2.tar.gz | sha256sum | cut -d ' ' -f 1
-```
 
+The `VERSION` environment variable is **required**.
+
+The (optional) arguments is list of tests that are run during the process. Each entry must be URL encoded (spaces, at least), and be in the format `command '=' expected_result`.  In the example above, the test will check that the version command for gotop returns the expected version tag.
+
+The action is performing these steps:
+
+1. Package the `aur/PKGBUILD` (source code) package, including updating the PKGBUILD hashes and .SRCINFO metadata
+2. Install the resulting package 
+3. Run any tests
+4. Publish the AUR
+5. Repeat for `aur-bin/PKGBUILD`
+
+The tool uses [aurpublish](https://github.com/eli-schwartz/aurpublish) to publish the packages.
--- arch-package/entrypoint.sh
+++ arch-package/entrypoint.sh
@@ -1,9 +1,107 @@
 #!/bin/bash
 #
+# REQUIRED: $VERSION must be exported
+# OPTIONAL: $_tests may be defined. BNF-ish is: command '=' expected-result
+#           Both command and expected-result should be URL encoded.
+#
 # $0 "gotop%20-V=$VERSION ls%20-1%20|%20wc%20-l=9"
-
-export VERSION=${GITHUB_REF##*/}
 
 export _tests=${@-""}
 
-./update.sh $VERSION
+# Helper to unencode tests
+function urldecode() {
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
+
+# Runs tests defined in the $_tests variable.
+function runTests() {
+    for test in $_tests; do
+        local t=$(echo "$test" | cut -d '=' -f 1)
+        local e=$(echo "$test" | cut -d '=' -f 2)
+        local command=$(urldecode "$t")
+        local expected=$(urldecode "$e")
+        local got=$(eval "$command")
+        if [[ $got != $expected ]]; then
+            echo "'$command' expected '$expected', got '$got'"
+            exit 1
+        fi
+    done
+}
+
+# Installs each package in the directory, executes any defined tests, and
+# uninstalls the package
+#
+# Call from within a directory containing packages
+function testPackages() {
+    arch=$(uname -m)
+    for f in *${arch}.pkg.tar.xz; do
+        # Install the package
+        pkn=$(echo $f | sed "s/\(^.*\)-${VERSION}.*/\1/")
+
+        sudo pacman -U --noconfirm $f
+        [[ $? -ne 0 ]] && exit 1
+
+        # Test that the program runs
+        res=$(runTests)
+        if [[ $res -ne 0 ]]; then
+            sudo pacman -R --noconfirm $pkn
+            exit 1
+        fi
+
+        # Uninstall the package
+        sudo pacman -R --noconfirm $pkn
+        [[ $? -ne 0 ]] && exit 1
+    done
+}
+
+# Update and build the packages.
+# Call from the directory containing the PKGBUILD
+function buildPackages() {
+    sed -i "s/^pkgver=.*/pkgver=$VERSION/; /^sha256sums/d; /^md5sums/d" PKGBUILD
+
+    makepkg -g >> PKGBUILD
+    [[ $? -ne 0 ]] && exit 1
+
+    makepkg --printsrcinfo > .SRCINFO
+    [[ $? -ne 0 ]] && exit 1
+
+    makepkg -f
+}
+
+
+# Main loop; enters aur and aur-bin, and in each:
+# 1. Builds the packages
+# 2. Tests the packages
+# 3. Publishes the packages
+function update() {
+    for d in aur aur-bin; do
+        pushd $d
+
+        # Build the packages
+        buildPackages
+        ex=$?
+        if [[ $ex -ne 0 ]]; then
+            popd
+            echo FAILED $d
+            exit $ex
+        fi
+
+        # Test the packages
+        testPackages
+        if [[ $ex -ne 0 ]]; then
+            popd
+            echo FAILED $d
+            exit $ex
+        fi
+        popd
+
+        aurpublish log $d
+    done
+}
+
+set -x
+update()
+set +x
+
+exit 0
